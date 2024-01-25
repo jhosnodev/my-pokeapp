633eef8ecbf38a7b2c56e6c8eb38d8bcfa33462b
jhosno <jhosno.dev@gmail.com>
2024-01-25 14:48:07 -0400

    Limpiando archivos por dafualt. Agregando assets y archivos de API

---
+++ src/api/fetchPomemons.js
@@ -0,0 +1,2 @@
+//! Traer todos los pokms de la 1ra gen
+//* https://unpkg.com/pokemons@1.1/pokemons.json
---
+++ src/api/fetchpokemon.js
@@ -0,0 +1,2 @@
+//! Data de un solo pokemon
+//* https://pokeapi.co/api/v2/pokemon/${nombre}
