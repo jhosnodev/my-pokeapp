b5d93f24e3afa2ba66839b0cdea459f1a25548a8
jhosno <jhosno.dev@gmail.com>
2024-01-25 15:17:55 -0400

    Seteando routes. creando paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Items from './pages/Items'
+import Pokemons from './pages/Pokemons'
+import Pokemon from './pages/Pokemon'
+import Type from './pages/Type'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-     
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/items" element={<Items />}/>
+        <Route path="/pokemons" element={<Pokemons />}/>
+        <Route path="/pokemons/:name" element={<Pokemon />}/>
+        <Route path="/type" element={<Type />}/>
+      </Routes>
+      <div className="App">
+        <header className="App-header">
+
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Pokemons.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Pokemons() {
+  return (
+    <div>pokemons</div>
+  )
+}
