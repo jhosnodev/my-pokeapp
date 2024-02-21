2f727aa2395271953ecb71f4eef80377534d32e8
jhosno <jhosno.dev@gmail.com>
2024-02-21 19:56:14 -0400

    Estilo de lista de pkms. Agregando otros assets

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import pokeball from '../assets/images/pokeball.png'
 import pikachu from '../assets/images/pikachu.png'
+import items from '../assets/images/items.png'
 import pointer from '../assets/images/pointer.png'
 import styles from './Footer.module.css'
 
@@ -9,15 +10,15 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <Link to={'/pokemons'} className={styles.footerLink}>
-        <img src={pokeball} alt="pokeball"  className={styles.footerIcon} />
+        <img src={pikachu} alt="Pikachu" className={styles.footerIcon} />
         Pokemons
       </Link>
       <Link to={'/items'} className={styles.footerLink}>
-        <img src={pikachu} alt="Pikachu"  className={styles.footerIcon}  />
+        <img src={pokeball} alt="pokeball" className={styles.footerIcon} />
         Items
       </Link>
       <Link to={'/map'} className={styles.footerLink}>
-        <img src={pointer} alt="maps"  className={styles.footerIcon}  />
+        <img src={pointer} alt="maps" className={styles.footerIcon} />
         Maps
       </Link>
     </footer>
--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,12 +1,27 @@
 import { useState } from "react"
 import { Header, Footer } from "../components"
+import styles from './pokemons.module.css'
+
+import Bulbasaur from '../assets/images/bulbasaur.gif'
+import { Link } from "react-router-dom"
 
 export default function Pokemons() {
   const [query, setQuery] = useState("")
   return (
     <>
       <Header query={query} setQuery={setQuery} />
-      <div>Pokemon (All)</div>
+      <main>Pokemon (All)
+        <article className="">
+          <Link to='/' className={styles.listItem}>
+            <img src={Bulbasaur} alt="001 - Bulbasaur" className={styles.listItemIcon} />
+            <div className={styles.listItemText}>
+              <span>Bulbasaur</span>{" "}
+              <span>001</span>
+            </div>
+          </Link>
+        </article>
+
+      </main>
       <Footer />
     </>
   )
